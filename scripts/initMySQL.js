9dc66a8c9b1c33f20c51494e29c0a7f7d569d9bd
alvy <alvyjudy@gmail.com>
2020-07-15 17:28:06 -0400

    add script to initialize database

--- scripts/initMySQL.js
+++ scripts/initMySQL.js
@@ -1,12 +1,23 @@
 require('dotenv').config();
 var mysql= require("mysql");
 
+
+
 var con = mysql.createConnection(
   {
-    host: process.env.HOST || 'localhost',
-    user: process.env.USER || 'root',
-    password: process.env.PW || '12345678'
+    host: process.env.SQLHOST || 'localhost',
+    user: process.env.SQLUSER || 'root',
+    password: process.env.SQLPW || '12345678'
   }
 );
 
-con.connect()
+let init = async () => {
+  await con.connect();
+  await con.query("DROP DATABASE IF EXISTS Api;");
+  await con.query("CREATE DATABASE Api;");
+  await con.query("USE Api;");
+  await con.query("CREATE TABLE user (email varchar(255), pw varchar(255));");
+  con.end();
+}
+
+init();
