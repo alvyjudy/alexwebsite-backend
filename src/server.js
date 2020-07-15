276a8add4659b78d592fd4feab9ccfefa9fcb5b4
alvy <alvyjudy@gmail.com>
2020-07-15 17:28:45 -0400

    add minimal register functionality

--- src/server.js
+++ src/server.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const path = require("path");
+const mysql= require("mysql");
 
 const app = express();
 
+const auth = {
+  host: process.env.SQLHOST || 'localhost',
+  user: process.env.SQLUSER || 'root',
+  password: process.env.SQLPW || '12345678'
+}
+
+let sqlCon = mysql.createConnection(auth);
+sqlCon.connect();
+
+
 app.get("/api/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../static/api.html"))
+  res.send("hello");
 });
+
+app.post('/api/register', express.json(), async (req, res) => {
+  let username = req.body.username;
+  let pw = req.body.password;
+  await sqlCon.query("USE Api;")
+  await sqlCon.query("INSERT INTO Api.user (email, pw) VALUES (?, ?);", [username, pw]);
+  res.send("done");
+})
 
 app.listen(process.env.PORT || 3002);
